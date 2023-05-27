380248417ee3b931843c06e68918cf8a4ace2a05
centek134 <centek134@wp.pl>
2023-05-27 14:57:58 +0200

    changes -added <Loader/> -deleted few imports -deleted light ref

--- src/App.jsx
+++ src/App.jsx
@@ -2,31 +2,29 @@
 /* eslint-disable no-undef */
 /* eslint-disable react/no-unknown-property */
 import "./App.css"
-import {useState, useLayoutEffect, useRef} from "react";
+import { useRef} from "react";
 import { Canvas } from "@react-three/fiber";
-import { PerspectiveCamera } from "@react-three/drei";
+import { PerspectiveCamera, Loader } from "@react-three/drei";
 import { House, PageContent, Hero } from "./components/index";
-import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/all";
 const App = () => {
   const camera = useRef();
-  const light = useRef();
 
   return (
   <main className="wrapper">
     <section className="wrapper__canvas">
       <Canvas shadows="soft">
-        <color attach="background" args = {["#ECF2F9"]} />
         <PerspectiveCamera ref={camera} rotation = {[-0.6,0,0]} makeDefault position = {[0,10,13]} />
-        <spotLight ref={light} intensity={0.7} castShadow position = {[0,10,2.9]}/>
+        <color attach="background" args = {["#ECF2F9"]} />
+        <spotLight intensity={0.7} castShadow position = {[0,10,2.9]}/>
         <ambientLight intensity={0.5}/>
-        <House light = {light} camera={camera}/>
+        <House camera={camera}/>
       </Canvas>
     </section>
     <section className="wrapper__page">
       <Hero/>
       <PageContent/>
     </section>
+    <Loader/>
   </main>
   );
 };
