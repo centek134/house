311a7a48293bc0bb47837e839cad49e172d37198
centek134 <centek134@wp.pl>
2023-05-28 22:50:49 +0200

    changes added angle to spotlight

--- src/App.jsx
+++ src/App.jsx
@@ -8,14 +8,14 @@ import { PerspectiveCamera, Loader } from "@react-three/drei";
 import { House, PageContent, Hero } from "./components/index";
 const App = () => {
   const camera = useRef();
-
   return (
   <main className="wrapper">
     <section className="wrapper__canvas">
       <Canvas shadows="soft">
         <PerspectiveCamera ref={camera} rotation = {[-0.6,0,0]} makeDefault position = {[0,10,13]} />
         <color attach="background" args = {["#ECF2F9"]} />
-        <spotLight intensity={0.7} castShadow position = {[0,10,2.9]}/>
+        <spotLight angle={2} intensity={0.7}  castShadow position = {[0,10,2.9]}/>
+        {/* <spotLight intensity={0.7} castShadow position = {[0,10,2.9]}/> */}
         <ambientLight intensity={0.5}/>
         <House camera={camera}/>
       </Canvas>
