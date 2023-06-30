b8b85a2ba8f2bf5cfc419a64d52dbcbde53bd180
centek134 <centek134@wp.pl>
2023-06-30 20:16:42 +0200

    changes deleted old comment + changed App.css import path

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-undef */
 /* eslint-disable react/no-unknown-property */
-import "./App.css"
+import "./styles/App.css";
 import { useRef} from "react";
 import { Canvas } from "@react-three/fiber";
 import { PerspectiveCamera, Loader } from "@react-three/drei";
@@ -15,7 +15,6 @@ const App = () => {
         <PerspectiveCamera ref={camera} rotation = {[-0.6,0,0]} makeDefault position = {[0,10,13]} />
         <color attach="background" args = {["#ECF2F9"]} />
         <spotLight angle={2} intensity={0.7}  castShadow position = {[0,10,2.9]}/>
-        {/* <spotLight intensity={0.7} castShadow position = {[0,10,2.9]}/> */}
         <ambientLight intensity={0.5}/>
         <House camera={camera}/>
       </Canvas>
