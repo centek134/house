f9b363bef0c06bbcbcc33d069d59b61dd0e50fe1
centek134 <centek134@wp.pl>
2024-07-22 09:38:58 +0200

    Data update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ export const Hero = () => {
     <section className="hero">
         <div className="hero__first">
             <h1 className="first__name">Tomasz Osuch</h1>
-            <p className="first__title">Frontend Developer | IT Student</p>
+            <p className="first__title">Frontend Developer</p>
         </div>
         <div className="hero__second">
           <p className="second__title">TOMASZOSUCH PORTFOLIO</p>
--- src/components/PageContent.jsx
+++ src/components/PageContent.jsx
@@ -19,7 +19,7 @@ export const PageContent = () => {
               <span className="intro-wrapper__number --pink-text">01</span>
           </article>
           <article className="section__detail-wrapper">
-          <p className="detail-wrapper__text">Hello there ✋! I'm a third year computer science student from Poland. I'm currently researching front-end solutions to provide a better web user experience.</p>
+          <p className="detail-wrapper__text">Hello there ✋! I'm an frontend developer from Poland. I'm currently researching front-end solutions to provide a better web user experience.</p>
           <p className="detail-wrapper__text">In my free time I like to read books 📚 most often I go for genres such as horror, fantasy. Working out is also a big part of my life, allows me to stay healthy and in shape 💪. Other than that I do like to play board games 🎲.</p>
           </article>
       </section>
@@ -39,10 +39,13 @@ export const PageContent = () => {
             <div className="detail">
               <h3 className="detail__header">Software skills</h3>
               <ul className="detail__skills-list">
-                <li className="list__item">HTML5, CSS3, SASS, BEM</li>
-                <li className="list__item">JavaScript, Typescript, Rest API, Three.js, Blender</li>
-                <li className="list__item">React, React Router</li>
-                <li className="list__item">Jira, Github, AEM, Agile methodology</li>
+                <li className="list__item">React, JavaScript ES6+, TypeScript, REST API</li>
+                <li className="list__item">React Three Drei, React, Three Fiber, Three.js, GSAP</li>
+                <li className="list__item">Blender 3.5, Photoshop, Figma</li>
+                <li className="list__item">HTML5, CSS3, SASS, BEM, RWD</li>
+                <li className="list__item">Webpack, Npm, Github</li>
+                <li className="list__item">CMS: Adobe Experience Manager (AEM)</li>
+                <li className="list__item">Working with: Confluence, JIRA, Slack, Agile: Scrum</li>
               </ul>
             </div>
             <div className="detail">
@@ -51,6 +54,7 @@ export const PageContent = () => {
                 <li className="list__item">Problem solving abilities</li>
                 <li className="list__item">Ability to work under time pressure</li>
                 <li className="list__item">Analytical thinking</li>
+                <li className="list__item">Willingness to learn</li>
                 <li className="list__item">Ability to work in a group</li>
               </ul>
             </div>
@@ -63,6 +67,15 @@ export const PageContent = () => {
                 <li className="list__item">Samsung Galaxy <a className="item__link" href="https://www.samsung.com/uk/smartphones/galaxy-z-flip5/" target="__blank" title="Link to samsung z flip5 website">Z Flip5</a> & <a className="item__link" href="https://www.samsung.com/uk/smartphones/galaxy-z-fold5/" target="__blank" title="Link to samsung z flip5 website">Z Fold5</a></li>
                 <li className="list__item">Samsung Galaxy <a className="item__link" href="https://www.samsung.com/uk/smartphones/galaxy-s24/" target="__blank" title="Link to samsung s24 & s24+ website">S24</a> & <a className="item__link" href="https://www.samsung.com/uk/smartphones/galaxy-s24-ultra/" target="__blank" title="Link to samsung s24 ultra website">S24 Ultra</a></li>
               </ul>
+              <small>
+
+              <ul className="detail__role-list">
+                <h4>My role during the projects:</h4>
+                <li >Preparation and onboarding of tools necessary for the project take-off</li>
+                <li >Creation and localization of websites for specific European markets under the HQ (Korean) guidelines</li>
+                <li >Solving complex technical problems occurring during production</li>
+              </ul>
+              </small>
             </div>
             <div className="detail">
               <h3 className="detail__header">Would you like to see more? Check my <a className="header__link" href="https://github.com/centek134" target="__blank">github</a>.</h3>
